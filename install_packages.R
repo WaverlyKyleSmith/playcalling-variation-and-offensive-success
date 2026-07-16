9f1856b587599561bd64f22d7e73eac0e0d040c8
Waverly (Kyle) Smith <wkylesmith3@gmail.com>
2026-07-16 00:34:21 -0400

    Add files via upload

---
+++ install_packages.R
@@ -0,0 +1,6 @@
+install.packages(c("tidyverse", "nflverse", "entropy", 
+                   "readxl", "dplyr", "lme4", "sandwich", 
+                   "lmtest", "car", "caret", "moments", 
+                   "modelsummary", "kableExtra"))
+
+#also required, a working LaTeX (`xelatex`) for PDF rendering
